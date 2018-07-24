f0a04e90867e42a447615108cb26aa2f9df2f902
Randy Pitcher <randypitcherii@gmail.com>
2018-07-24 11:59:46 -0500

    improved login routing

--- ui/heirloom_ui/routes/login.py
+++ ui/heirloom_ui/routes/login.py
@@ -8,12 +8,21 @@ from heirloom_ui.forms.login import LoginForm
 bp = Blueprint('login', __name__)
 
 #set default route
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/')
+def index():
+  isLoggedIn = False # TODO: do this for real
+  if isLoggedIn():
+    return redirect('/home')
+  else:
+    return redirect('/login')
+
+#set login route
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
   if form.validate_on_submit():
     flash('Login requested for user {}, remember_me={}'.format(
-      form.username.data, form.remember_me.data))
+        form.username.data, form.remember_me.data))
     return redirect('/home')
   return render_template('login.html', title='Heirloom Login', form=form)
 #==============================================================================
