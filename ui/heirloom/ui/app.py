3461c9600aea6a5cf8ef4eb03b43499bd0c03c4e
Randy Pitcher <randypitcherii@gmail.com>
2018-07-24 18:09:11 -0500

    removed flask restplus because it is buggy and wastes time

--- ui/heirloom/ui/app.py
+++ ui/heirloom/ui/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
-
 from heirloom.ui.config import Config
-from heirloom.ui.routes.restplus import restplus_api
 from heirloom.ui.routes.login import bp as login_blueprint
 from heirloom.ui.routes.home import bp as home_blueprint
 
@@ -16,11 +14,6 @@ app.config.from_object(Config)
 
 # Setup database
 db = SQLAlchemy(app)
-
-# register restplus with flask
-blueprint = Blueprint('api', __name__)
-restplus_api.init_app(blueprint)
-app.register_blueprint(blueprint, url_prefix='/api')
 
 # register blueprint modules
 app.register_blueprint(login_blueprint) # default route
