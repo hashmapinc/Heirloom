d3da6cb2ee5ed3537fc3ae8a20656f9edd8d8f2f
Randy Pitcher <randypitcherii@gmail.com>
2018-07-25 15:00:37 -0500

    improved make for venv usage

--- ui/scripts/runContainer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run \
-    --rm \
-    -it \
-    -p 80:80 \
-    -v /Users/randypitcherii/projects/Heirloom/ui/heirloom:/mnt/heirloom \
-    hashmapinc/heirloom-ui  /bin/bash
--- ui/scripts/runDevServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export FLASK_APP=heirloom/ui/main.py
-export FLASK_DEBUG=1
-python3 -m flask run --port 8123 --host '0.0.0.0'
