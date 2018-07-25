15bbe9052814dd7812a035ae108ddf822152ef17
Randy Pitcher <randypitcherii@gmail.com>
2018-07-25 15:11:40 -0500

    added intelligent login routing from home route

--- ui/heirloom/ui/routes/home.py
+++ ui/heirloom/ui/routes/home.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
+from flask_login import current_user
 
 #==============================================================================
 # home API 
@@ -9,5 +10,9 @@ bp = Blueprint('home', __name__)
 #set default route
 @bp.route('/')
 def home():
+  # if not signed in, reroute to login
+  if not current_user.is_authenticated:
+    return redirect('/login')
+  
   return render_template('home.html', title='Heirloom Home', username='USER')
 #==============================================================================
