ad9c6eb36105f10277650f7572bd69074b54dd0f
Randy Pitcher <randypitcherii@gmail.com>
2018-07-26 13:54:47 -0500

    finished signup logic

--- ui/heirloom/ui/routes/signup.py
+++ ui/heirloom/ui/routes/signup.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import current_user
 
+from heirloom.ui import db
 from heirloom.ui.forms.signup import SignupForm
 from heirloom.ui.models.user import User
 
@@ -10,18 +11,23 @@ from heirloom.ui.models.user import User
 # define namespace
 bp = Blueprint('signup', __name__)
 
-# set login route
+# set default route
 @bp.route('/', methods=['GET', 'POST'])
 def signup():
   # if the user is logged in, redirect home
   if current_user.is_authenticated:
     return redirect(url_for('home.home'))
 
-  # validate login form
+  # validate signup form
   form = SignupForm()
   if form.validate_on_submit():
-    # form is valid, get user
-    print("valid form")
+    # form is valid, create the user
+    user = User(username=form.username.data, email=form.email.data, role=form.role.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations, you are now a registered user!')
+    return redirect(url_for('login.login'))
 
   else:
     # handle an invalid form
